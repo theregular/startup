211cfda2ca0f0d867100f1d79c0413e91fc3ceea
Ben Kartchner <brkartch@gmail.com>
2023-02-23 18:49:34 -0700

    login button links to profile

---
+++ eraseAndDeploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+while getopts k:h: flag
+do
+    case "${flag}" in
+        k) key=${OPTARG};;
+        h) hostname=${OPTARG};;
+    esac
+done
+
+if [[ -z "$key" || -z "$hostname" ]]; then
+    printf "\nMissing required parameter.\n"
+    printf "  syntax: eraseAndDeploy.sh -k <pem key file> -h <hostname>\n\n"
+    exit 1
+fi
+
+printf "\n----> Deploying root website to $hostname with $key\n-------------------------------\n"
+
+# Step 1
+printf "\n----> Clear out the previous distribution on the target.\n"
+ssh -i "$key" ubuntu@$hostname << ENDSSH
+rm -rf services/public
+mkdir -p services/public
+ENDSSH
+
+# Step 2
+printf "\n----> Copy the distribution package to the target.\n"
+scp -r -i "$key" * ubuntu@$hostname:services/public
