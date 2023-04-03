3b053dcc375e4a8c218c91f89d6409796a1519e8
Ben Kartchner <brkartch@gmail.com>
2023-04-02 19:11:06 -0600

    back to normal

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const DB = require('./database.js'); //code for database
 const bcrypt = require('bcrypt'); //encryption module
+const handlebars = require('express-handlebars'); //express-handlebars
 const cookieParser = require('cookie-parser');
 const app = express();
+
+//set for handlebars
+app.set('view engine', 'handlebars');
+
+app.engine('handlebars', handlebars.engine({
+    layoutsDir: `${__dirname}/views/layouts`
+}));
+
 
 // The service port. In production the application is statically hosted by the service on the same port.
 const port = process.argv.length > 2 ? process.argv[2] : 3000;
@@ -12,6 +21,35 @@ app.use(express.json());
 
 // Serve up the application's static content
 app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/public/index.html');
+})
+
+app.get('/register', (req, res) => {
+  res.sendFile(__dirname + '/public/register.html');
+})
+
+app.get('/login', (req, res) => {
+  res.sendFile(__dirname + '/public/login.html');
+})
+
+
+app.get('/profile/test', (req, res) => {
+  const username = 'tester';
+
+
+
+  res.render('profile', {layout: 'main', username: username})
+})
+
+//:username will change based on request input
+app.get('/profile/:username', (req, res) => {
+  const username = req.params.username;
+
+  res.render('profile', {layout: 'main', username: username});
+})
+
 
 // Router for service endpoints
 var apiRouter = express.Router();
@@ -26,7 +64,7 @@ apiRouter.post('/auth/create', async (req, res) => {
 
     // Set the cookie
     //setAuthCookie(res, user.token);
-
+    
     res.send({
       id: user._id,
     });
@@ -60,19 +98,22 @@ secureApiRouter.use(async (req, res, next) => {
   }
 });
 
-
 // Default error handler
 app.use(function (err, req, res, next) {
-    res.status(500).send({ type: err.name, message: err.message });
-  });
+  res.status(500).send({ type: err.name, message: err.message });
+});
   
-  // Return the application's default page if the path is unknown
-  app.use((_req, res) => {
-    res.sendFile('index.html', { root: 'public' });
-  });
+// Return an error page if page not found
+app.use((req, res) => {
+    //res.sendFile('index.html', { root: 'public' });
+  res.send('ERROR page not found'); //update to actual HTML error page
+});
 
-  // setAuthCookie in the HTTP response
+
+
   
+  
+// setAuthCookie in the HTTP response
 function setAuthCookie(res, authToken) {
   res.cookie(authCookieName, authToken, {
     secure: true,
