2a88e215931f0420d396e62483394d046581df89
David Hernandez <dhernandezcollege@gmail.com>
2023-04-04 16:50:02 -0600

    Removed all the code that was commented out to make the code more readable

--- public/login.js
+++ public/login.js
@@ -1,22 +1,3 @@
-//get username from local storage cookie //GET WORKING
-/*
-(async () => {
-    let authenticated = false;
-    const userName = localStorage.getItem('username');
-
-    //find stored username in DB
-    if (userName) {
-      const user = await getUser(userName);
-      authenticated = user?.authenticated;
-    }
-    
-    //if already in local storage redirect to user's page (add authtoken confirmation?)
-    if (authenticated) {
-      window.location.pathname = `/profile/${userName}`;
-    }
-  })();
-  */
-
 async function loginUser() {
   const usernameInput = document.querySelector('#userName').value.trim();
   const passwordInput = document.querySelector('#userPassword').value.trim();
@@ -35,18 +16,10 @@ async function loginUser() {
     const body = await response.json();
 
     if (response?.status === 200) {
-      //alert('Login Succeeded!');
       localStorage.setItem('username', usernameInput);
       window.location.pathname = `/profile/${usernameInput}`;
     } else {
-      //alert('Login failed!');
       valid = 'Incorrect username or password!';
-      /*
-      const modalEl = document.querySelector('#msgModal');
-      modalEl.querySelector('.modal-body').textContent = `⚠ Error: ${body.msg}`;
-      const msgModal = new bootstrap.Modal(modalEl, {});
-      msgModal.show();
-      */
     }
   }
   if (valid !== true){
@@ -58,26 +31,11 @@ async function loginUser() {
 function validate(username, password) {
 
   if (username === '') {
-  //alert('Please enter a username');
   return 'Please enter a username';
   }
 
   if (password === '') {
-  //alert('Please enter a password');
   return 'Please enter a password';
   }
-
   return true;
 }
-
-
-
-/*
-document.getElementById("login").addEventListener("click", displayMessage);
-
-
-function displayMessage() {
-    alert ("Loggin You In, Please Wait")
-}
-*/
- 
